30f88ebda6372c85c24d78350de62bc95b79b67a
Dr. Noemie Pouros <133541780+ahmadrakiz@users.noreply.github.com>
2025-08-26 14:31:18 +0200

    Create script.js
    Add script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// زر إنشاء البريد المؤقت
+document.querySelector("button").addEventListener("click", () => {
+    // هنا يمكنك استدعاء API خارجي لتوليد البريد المؤقت
+    alert("هنا سيتم إنشاء البريد المؤقت عبر API");
+});
